269b44353aee77eca599c7837594cdcaa315aff8
Cody Sheridan <csheridan994@gmail.com>
2018-04-12 10:38:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,8 @@ function destructivelyPrependKitten(name) {
 function destructivelyRemoveLastKitten(name) {
   return kittens.pop();
 }
+
+function destructivelyRemoveFirstKitten(name) {
+  return kittens.shift();
+}
+
