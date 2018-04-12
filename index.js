fbbce069dfea4e73b9b1091ae361cf1483a8a740
Cody Sheridan <csheridan994@gmail.com>
2018-04-12 10:42:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,3 +23,7 @@ function appendKitten(name) {
 function prependKitten(name) {
   return ["Arnold", ...kittens];
 }
+
+function removeLastKitten(name) {
+  return kittens.slice(0,kittens.length-1);
+}
